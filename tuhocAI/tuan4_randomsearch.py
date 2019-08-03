2da9bc588b4988ea33de51c4bd1670b2343fe564
tranngocanh91 <53486510+tranngocanh91@users.noreply.github.com>
2019-08-03 22:53:04 +0700

    hoc_AI

---
+++ tuhocAI/tuan4_randomsearch.py
@@ -0,0 +1,7 @@
+def generate_vector(n=10,m=8):
+    vector =[[0]*n for _ in range(m)]
+    for i in range (m):
+        for j in range (n):
+            if random.random()>=0.5:
+                vector[i][j]=1
+    return vector
